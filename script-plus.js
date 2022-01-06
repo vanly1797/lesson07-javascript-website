c1c3e3c2278dc95319a5bea30191ea5645e3639d
unknown <xuongvan.1797@gmail.com>
2022-01-06 11:46:08 +0700

    update code javascript-plus

---
+++ script-plus.js
@@ -0,0 +1,22 @@
+// Bài 01 - START
+var a = prompt("Nhập vào cạnh a: ");
+var b = prompt("Nhập vào cạnh b: ");
+var c = prompt("Nhập vào cạnh c: ");
+var p = a + b + c;
+console.log(p);
+// Bài 01 - END
+
+// Bài 02 - START
+// let _baihat = "Hãy trao cho anh hãy trao cho anh Hãy trao cho anh thứ anh đang mong chờ (oh oh oh oh) Hãy trao cho anh hãy trao cho anh Hãy mau làm điều ta muốn vào khoảnh khắc này đê (oh oh oh oh)"
+// let _xoakt = _baihat.replace(/\s/g, '');
+// console.log(_xoakt.length);
+// console.log(_baihat.toUpperCase());
+// console.log(_baihat.toLowerCase());
+// console.log(_baihat.replace(/anh/g, "em"));
+// Bài 02 - END
+
+// Bài 03 - START
+let x = 10;
+// a) x++ - --x + x-- => x++ = 10,  --x = 9,  x-- = 9
+// b) ++x - x++ + x-- + x++ =>  ++x = 11, x++ = 11, x-- = 11, x++ = 11
+// Bài 03 - END
