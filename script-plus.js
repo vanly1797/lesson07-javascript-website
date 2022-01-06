7b74fb124bb89e7f7153fb0304985fa1a93b7fa6
unknown <xuongvan.1797@gmail.com>
2022-01-06 17:42:52 +0700

    fix bug

--- script-plus.js
+++ script-plus.js
@@ -1,18 +1,28 @@
 // Bài 01 - START
-var a = prompt("Nhập vào cạnh a: ");
-var b = prompt("Nhập vào cạnh b: ");
-var c = prompt("Nhập vào cạnh c: ");
-var p = a + b + c;
-console.log(p);
+let a = prompt("Nhập vào cạnh a: ");
+let b = prompt("Nhập vào cạnh b: ");
+let c = prompt("Nhập vào cạnh c: ");
+let p = Number(a) + Number(b) + Number(c);
+console.log("Chu vi tam giác = " + p);
+let nuacv = (p / 2).toFixed(1);
+console.log(nuacv);
+let temp = (nuacv * (nuacv - Number(a)) * (nuacv - Number(b)) * (nuacv - Number(c)));
+console.log("Diện tích tam giác = " + Math.sqrt(temp).toFixed(2));
+let bk = ((Number(a) * Number(b) * Number(c)) / (4 * Math.sqrt(temp).toFixed(2)));
+console.log("Bán kính đường tròn ngoại tiếp = " + bk);
+
+const pi = 3.14;
+let dtht = (bk * bk * pi);
+console.log("Diện tích hình tròn = " + dtht.toFixed(2));
 // Bài 01 - END
 
 // Bài 02 - START
-// let _baihat = "Hãy trao cho anh hãy trao cho anh Hãy trao cho anh thứ anh đang mong chờ (oh oh oh oh) Hãy trao cho anh hãy trao cho anh Hãy mau làm điều ta muốn vào khoảnh khắc này đê (oh oh oh oh)"
-// let _xoakt = _baihat.replace(/\s/g, '');
-// console.log(_xoakt.length);
-// console.log(_baihat.toUpperCase());
-// console.log(_baihat.toLowerCase());
-// console.log(_baihat.replace(/anh/g, "em"));
+let _baihat = "Hãy trao cho anh hãy trao cho anh Hãy trao cho anh thứ anh đang mong chờ (oh oh oh oh) Hãy trao cho anh hãy trao cho anh Hãy mau làm điều ta muốn vào khoảnh khắc này đê (oh oh oh oh)"
+let _xoakt = _baihat.replace(/\s/g, '');
+console.log(_xoakt.length);
+console.log(_baihat.toUpperCase());
+console.log(_baihat.toLowerCase());
+console.log(_baihat.replace(/anh/g, "em"));
 // Bài 02 - END
 
 // Bài 03 - START
--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 var a = prompt("Nhập vào giá trị a: ")
 var b = prompt("Nhập vào giá trị b: ")
 
-console.log("Căn bậc hai của a = " + Math.sqrt(a));
-console.log("Căn bậc hai của b = " + Math.sqrt(b));
+console.log("Căn bậc hai của a = " + Math.sqrt(Number(a)));
+console.log("Căn bậc hai của b = " + Math.sqrt(Number(b)));
 //Bài 01 - END
 
 //Bài 02 - START
 var cd = prompt("Nhập vào độ dài cạnh đáy: ");
 var ch = prompt("Nhập vào chiều cao: ");
-var dt = (cd * ch) / 2;
+var dt = ((Number(cd) * Number(ch)) / 2);
 console.log("Diện tích tam giác = " + dt);
 //Bài 02 - END
 
@@ -18,8 +18,8 @@ const temp1 = 32;
 const temp2 = 1.8;
 var do1 = prompt("Nhập vào độ F thứ nhất: ");
 var do2 = prompt("Nhập vào độ F thứ hai: ");
-var doc1 = (do1 - temp1) / temp2;
-var doc2 = (do2 - temp1) / temp2;
+var doc1 = ((Number(do1) - temp1) / temp2);
+var doc2 = ((Number(do2) - temp1) / temp2);
 
 console.log("Độ C thứ nhất = " + doc1.toFixed(1));
 console.log("Độ C thứ hai = " + doc2.toFixed(1));
@@ -30,8 +30,8 @@ const temp3 = 1000;
 
 var km1 = prompt("Nhập vào số km thứ nhất: ");
 var km2 = prompt("Nhập vào số km thứ hai: ");
-var m1 = km1 * temp3;
-var m2 = km2 * temp3;
+var m1 = (Number(km1) * temp3);
+var m2 = (Number(km2) * temp3);
 
 console.log("Số m thứ nhất sau khi chuyển đổi = " + m1);
 console.log("Số m thứ hai sau khi chuyển đổi = " + m2);
